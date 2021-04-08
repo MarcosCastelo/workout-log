c7d314926ad799955e5c5e9c1fc18f2e2dd7fdad
MarcosCastelo <marcosvitorcastelo@gmail.com>
2021-04-08 18:50:20 -0300

    Creating component for layout

---
+++ src/components/Layout/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+import UserInput from '../UserInput';
+
+
+export default function Layout() {
+  return (
+    <Container>
+      <UserInput />
+      
+    </Container>
+  );
+}
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  *, button, input {
+    font-family: sans-serif;
+  }
+
+  :root {
+    --primary: #79031D;
+    --secondary: #EDB518;
+    --white: #F5F7F7;
+    --black: #000407;
+  }
+`;
