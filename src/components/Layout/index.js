d023ead80a35421ca97a68e4526fb7f351d612b3
MarcosCastelo <marcosvitorcastelo@gmail.com>
2021-04-09 03:19:41 -0300

    adding behaviour of user input component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { Component, useEffect } from 'react';
 
 import { Container } from './styles';
 
 import UserInput from '../UserInput';
+import ExerciseList from '../ExerciseList';
 
 
-export default function Layout() {
-  return (
-    <Container>
-      <UserInput />
-      
-    </Container>
-  );
+class Layout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: []
+    }
+
+
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  handleInput(value) {
+    this.setState((prevState) => {
+      const list = prevState.list.concat(value);
+      const json = JSON.stringify(list);
+      localStorage.setItem("list", json);
+      return { list: list };
+    });
+  }
+  
+
+  componentDidMount() {
+      const json = localStorage.getItem("list");
+      const savedList = JSON.parse(json);
+      if (savedList) {
+        this.setState(() => {
+          return { list: savedList };
+        })
+      }
+  }
+  render() {
+    return (
+      <Container>
+        <UserInput handleInput={this.handleInput} />
+        <ExerciseList entries={this.state.list} />
+      </Container>
+    );
+  }
 }
+
+export default Layout;
