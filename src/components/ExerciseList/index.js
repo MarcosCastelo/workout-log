3bf2a0cd9f54c15b36330702b89ba5d3f9f0ca9c
MarcosCastelo <marcosvitorcastelo@gmail.com>
2021-04-09 17:59:30 -0300

    Changing pure html to material ui components

--- src/components/ExerciseList/index.js
+++ src/components/ExerciseList/index.js
@@ -1,43 +1,69 @@
 import React, { Component } from 'react';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
 
 import { Board } from './styles';
 
 class ExerciseList extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      rows: []
+    }
 
-    this.renderSingleItem = this.renderSingleItem.bind(this);
+    this.columns = [
+      {
+        field: 'time',
+        headerName: 'Time',
+        width: 130,
+        valueGetter: (params) => `${params.getValue('time')}h`
+      },
+      { field: 'exercise', headerName: 'Exercise', width: 130 },
+      { field: 'date', headerName: 'Date', width: 130 },
+    ];
+    this.getRows = this.getRows.bind(this);
   }
 
-  
+  getRows() {
+    return this.props.entries;
+  }
 
-  renderSingleItem(item) {
-    return (
-      <tr key={item.key}>
-        <td>{item.time}h</td>
-        <td>{item.exercise}</td>
-        <td>{item.date}</td>
-        <td><button onClick={() => this.props.deleteItem(item.key)}>Delete</button></td>
-      </tr>
-    )
+
+  componentDidCatch() {
+    this.setState(this.props.entries);
+    console.log(this.state)
   }
 
   render() {
-    var exerciseEntries = this.props.entries;
-    console.log(exerciseEntries);
-    var listExercises = exerciseEntries.map(this.renderSingleItem);
-
     return (
       <Board>
-        <table>
-          <tr>
-            <th>Time</th>
-            <th>Exercise Type</th>
-            <th>Date</th>
-            <th></th>
-          </tr>
-          {listExercises}
-        </table>
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableCell sortDirection="asc">Time</TableCell>
+              <TableCell>Exercise</TableCell>
+              <TableCell>Date</TableCell>
+              <TableCell></TableCell>
+            </TableHead>
+            <TableBody>
+              {this.props.entries.map((row) => (
+                <TableRow key={row.id}>
+                  <TableCell> {row.time}h</TableCell>
+                  <TableCell>{row.exercise}</TableCell>
+                  <TableCell>{row.date}</TableCell>
+                  <TableCell><Button onClick={() => this.props.deleteItem(row.id)}><DeleteOutlineIcon /></Button></TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Board>
     );
   }
